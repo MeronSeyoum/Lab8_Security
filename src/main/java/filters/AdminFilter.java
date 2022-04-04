d3f7d07de509b3c10ab9cb00a12f90fd66ae968a
Meron Seyoum <meryato@gmail.com>
2022-04-04 11:29:04 -0600

    Admin Filter class is added

---
+++ src/main/java/filters/AdminFilter.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Beans/Bean.java to edit this template
+ */
+package filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ *
+ * @author Meron Seyoum
+ */
+@WebFilter(filterName = "AdminFilter", servletNames = {"AdminServlet"})
+public class AdminFilter implements Filter {
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+}
+     /**
+     * Destroy method for this filter
+     */
+    public void destroy() {        
+    }
+
+    /**
+     * Init method for this filter
+     */
+    public void init(FilterConfig filterConfig) {          
+    }   
+}
